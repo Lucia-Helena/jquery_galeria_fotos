cb464716a8f06922cee78d968c4e015fed053752
Lúcia Helena <luciahproenca@gmail.com>
2023-12-07 20:21:00 -0200

    Adicionei imagens e fiz alguns ajustes no código

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('cancel-button').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+      e.preventDefault(); 
+      const  newImageAddress = $('endereco-img-nova').val();
+      const newItem = $('<li></li>');
+      $(`<img src= "${newImageAddress}"/>`).appendTo(newItem);
+      $(`
+      
+            <div class="overlay-img-link">
+            <a href="${newImageAddress}" title="Ver imagem em tamanho real" target="_blank">
+            Ver imagem em tamanho real
+        </a>
+            </div>`
+      ).appendTo(newItem);
+      $(newItem).appendTo('ul');                                                                                                                                                  
+    
+    })
+
+})
